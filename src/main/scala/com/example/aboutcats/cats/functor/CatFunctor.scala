ec017deba07605ee927bbd75071e08c0d108ae2b
onionyuyixi <qingluanxi@gmail.com>
2020-10-08 23:26:03 +0800

    add  imap

--- src/main/scala/com/example/aboutcats/cats/functor/CatFunctor.scala
+++ src/main/scala/com/example/aboutcats/cats/functor/CatFunctor.scala
@@ -165,6 +165,53 @@ object CatFunctor extends App {
   value(Box("123")) // 采用的便是 implicit def boxMethod
   Box("123").boxMethod // 采用的便是 implicit class BoxMethodClass
 
+  // Invariant functors and the imap method  example
+
+
+  trait Codec[A] {
+    self =>
+    def encode(value: A): String
+
+    def decode(value: String): A
+
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+      override def encode(value: B): String = self.encode(enc(value))
+
+      override def decode(value: String): B = dec(self.decode(value))
+    }
+  }
+
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+
+  def decode[A](value: String)(implicit c: Codec[A]): A = c decode value
+
+  implicit val stringCodec = new Codec[String] {
+    override def encode(value: String): String = value
+
+    override def decode(value: String): String = value
+
+  }
+
+  implicit val intCodec: Codec[Int] = stringCodec.imap(_.toInt, _.toString)
+  implicit val doubleCodec: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
+
+  implicit val booleanCodec: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
+
+  implicit def boxCodec[B](implicit c: Codec[B]): Codec[Box[B]] = new Codec[Box[B]] {
+    override def encode(bb: Box[B]): String = c.encode(bb.value)
+
+    override def decode(value: String): Box[B] = Box(c.decode(value))
+
+  }
+
+  println(encode(123.4))
+  println(decode[Double]("123.4"))
+  println(encode(Box(123)))
+  println(decode[Box[Double]]("123.4"))
+
+
+  // cat Invariant functors implement a method called imap that is informally equivalent to a combination of map and contramap
+
 }
 
 trait MyList[A] {
